d58b303d970400f3172e3283d2811f28b8a0b5b3
Ray Pulido <ray_pulido@outlook.com>
2021-04-03 18:26:14 -0600

    Nuevo Orden

--- ProcesImagen/Filtro/principal.c
+++ ProcesImagen/Filtro/principal.c
@@ -118,33 +118,7 @@ void GraytoRGB(unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t wid
          }   
 }
 
-// void GraytoRGB(unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width,uint32_t height ){
-// int indiceGray,indiceRGB;
 
-//      for(register int y = 0;y < height; y++){
-//          for(register int x = 0; x < width; x++){
-//              indiceGray = (y*width) + x;
-//              indiceRGB = indiceGray * 3;
-//              imagenRGB[indiceRGB] = imagenGray[indiceGray];
-//              imagenRGB[indiceRGB+1] = imagenGray[indiceGray];
-//              imagenRGB[indiceRGB+2] = imagenGray[indiceGray];
-//          }   
-//    } 
-// }
-// void RGBToGray(unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width,uint32_t height ){
-//     int indiceGray,indiceRGB;
-//     unsigned char nivelGray;
-//     for(register int y = 0;y < height; y++){    
-//         for(register int x = 0; x < width; x++){
-//             indiceGray = (y*width) + x;
-//             indiceRGB = indiceGray * 3;
-//             // Por promedio nivelGray = (*(imagenRGB+ indiceRGB +0) + *(imagenRGB+ indiceRGB+1) + *(imagenRGB + indiceRGB+2)) /3;
-//             // Por ponderacion 
-//             nivelGray = (30* (*(imagenRGB+ indiceRGB +0)) + 59* (*(imagenRGB+ indiceRGB+1)) + 11* (*(imagenRGB + indiceRGB+2)) ) /100;
-//             imagenGray[indiceGray] = nivelGray;
-//         }
-//     }
-// }
 void RGBToGray(unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width,uint32_t height ){
     unsigned char nivelGray;
      
